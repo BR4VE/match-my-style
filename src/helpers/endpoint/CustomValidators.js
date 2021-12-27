19b808ed81bc989af925ae24663c850c133fd736
Mert Batmazoglu <mertbatmazoglu@gmail.com>
2021-12-27 17:42:16 +0300

    Add custom validator for categories

--- src/helpers/endpoint/CustomValidators.js
+++ src/helpers/endpoint/CustomValidators.js
@@ -1,8 +1,31 @@
+import ArrayUtils from "helpers/utils/ArrayUtils";
 import BadRequestError from "errors/BadRequestError";
+import { ClothesConstants } from "models/ClothesModel";
 import ErrorMessages from "helpers/utils/ErrorMessages";
 import Validator from "helpers/Validator";
 
 export default {
+  categories: {
+    options: (categories) => {
+      const isArray = Array.isArray(categories);
+      const hasLength = !!categories?.length;
+
+      if (!isArray || !hasLength) {
+        throw new BadRequestError(ErrorMessages.invalid("Categories"));
+      }
+
+      const contains = ArrayUtils.contains(
+        categories,
+        ClothesConstants.categories
+      );
+
+      if (!contains) {
+        throw new BadRequestError(ErrorMessages.invalid("Categories"));
+      }
+
+      return true;
+    },
+  },
   phoneNumber: {
     options: (phoneNumber) => {
       const isPhoneNumber = Validator.validatePhoneNumber(phoneNumber);
