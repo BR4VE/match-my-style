15e1c9bed4618e1f4a8b52db38c87d02af040565
Mert Batmazoglu <mertbatmazoglu@gmail.com>
2021-12-28 16:44:23 +0300

    Add findOneRandomBy method

--- src/services/model/ClothesModelService.js
+++ src/services/model/ClothesModelService.js
@@ -1,6 +1,14 @@
 import BaseModelService from "services/model/BaseModelService";
 import ClothesModel from "models/ClothesModel";
 
-class ClothesModelService extends BaseModelService {}
+class ClothesModelService extends BaseModelService {
+  async findOneRandomBy(fields) {
+    const [result] = await this.model.aggregate([
+      { $match: fields },
+      { $sample: { size: 1 } },
+    ]);
+    return result;
+  }
+}
 
 export default new ClothesModelService(ClothesModel);
