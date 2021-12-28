720d1fd9c8f12a017b91b1cf647ca92a0b997a8a
Mert Batmazoglu <mertbatmazoglu@gmail.com>
2021-12-28 17:25:21 +0300

    Add findUserCombines method

--- src/services/model/CombineModelService.js
+++ src/services/model/CombineModelService.js
@@ -1,6 +1,8 @@
 import BaseModelService from "services/model/BaseModelService";
 import CombineModel from "models/CombineModel";
 
+const LimitCount = 10;
+
 class CombineModelService extends BaseModelService {
   async findIsCreated(user, clothesIds) {
     const existingCombine = await this.model.findOne({
@@ -8,6 +10,17 @@ class CombineModelService extends BaseModelService {
       clothesIds: { $all: clothesIds },
     });
     return !!existingCombine;
+  }
+
+  async findUserCombines(user, options = {}) {
+    const { page = 1 } = options;
+    return this.model
+      .find({
+        userId: user._id,
+      })
+      .sort({ createdAt: -1 })
+      .skip((page - 1) * LimitCount)
+      .limit(LimitCount);
   }
 }
 
